89088df7b179782fa4b3c2ad329361ae219b501b
centriojs <centriojs@gmail.com>
2018-12-15 17:13:10 +0800

    Set filesystem in directory test case

---
+++ test/filesystem.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('chai').assert,
+    path = require('path');
+
+require('../lib/load');
+
+global.ABSPATH = __dirname;
+
+describe('Filesystem: Directory', () => {
+    let dir = 'test-area';
+
+    it('mkDir: Should create a new directory', done => {
+        mkDir(dir)
+            .then( ok => {
+                assert.isOk(ok, true);
+                done();
+            })
+            .catch( done );
+    });
+
+
+    it('rmDir: Should delete `test-area` directory.', done => {
+        rmDir(dir)
+            .then( ok => {
+                assert.isOk( ok, true );
+                done();
+            })
+            .catch(done);
+    });
+});
