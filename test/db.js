e52b7e177af23c9e9bc99cc85317cf82e178a423
centriojs <centriojs@gmail.com>
2018-12-17 13:00:18 +0800

    Update MySQL query cycle.

--- test/db.js
+++ test/db.js
@@ -4,15 +4,16 @@ const assert = require('chai').assert;
 
 require('../lib/load');
 
-let {dbManager} = require('../lib/db');
+let {dbManager, createDBTable} = require('../lib/db');
+
+let config = {
+    database: 'mysql',
+    dbName: 'dbxhub',
+    dbUser: 'dbxuser',
+    dbPass: 'dbxpass'
+};
 
 describe('MySQL database connection', () => {
-    let config = {
-        database: 'mysql',
-        dbName: 'dbxhub',
-        dbUser: 'dbxuser',
-        dbPass: 'dbxpass'
-    };
 
     it('Connect to the database', done => {
         dbManager(config)
@@ -33,4 +34,41 @@ describe('MySQL database connection', () => {
             })
             .catch(done);
     });
+});
+
+describe('MySQL table cycle', () => {
+    it('Create new table', done => {
+        createDBTable({
+            name: 'test1',
+            columns: {
+                ID: {
+                    type: 'BIGINT',
+                    length: 10,
+                    required: true,
+                    primary: true,
+                    increment: true
+                },
+                name: {
+                    type: 'VARCHAR',
+                    required: true
+                }
+            }
+        })
+            .then( ok => {
+                assert.isOk(ok, true);
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Get the table structure', done => {
+        dbManager(config)
+            .query('test1')
+            .getColumnsStructure()
+            .then( structure => {
+                console.log(structure);
+                done();
+            })
+            .catch(done);
+    })
 });
