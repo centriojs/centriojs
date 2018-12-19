c39039dd628fe3dce6ca8df19db94b0671c6ef3d
centriojs <centriojs@gmail.com>
2018-12-20 00:33:41 +0800

    Update mysql user queries

--- test/install-mysql.js
+++ test/install-mysql.js
@@ -8,9 +8,9 @@ const {DatabaseManager} = require('../lib/db');
 
 let config = {
     database: 'mysql',
-    dbName: 'dbxhub',
-    dbUser: 'dbxuser',
-    dbPass: 'dbxpass',
+    dbName: 'centriojs_test',
+    dbUser: 'root',
+    dbPass: 'root',
     secretKey: '52071d25e7ec91dd36bdd5166f01659f'
 };
 
--- test/user-mysql.js
+++ test/user-mysql.js
@@ -8,9 +8,9 @@ const {DatabaseManager} = require('../lib/db');
 
 let config = {
     database: 'mysql',
-    dbName: 'dbxhub',
-    dbUser: 'dbxuser',
-    dbPass: 'dbxpass',
+    dbName: 'centriojs_test',
+    dbUser: 'root',
+    dbPass: 'root',
     secretKey: '52071d25e7ec91dd36bdd5166f01659f'
 };
 
@@ -45,7 +45,7 @@ describe('MySQL: User Query', () => {
             .catch(done);
     });
 
-    it('Should get user where email is `nazzylove@local.dev`', done => {
+    it('Should get user where email is `nazzy@local.dev`', done => {
         getUserBy( 'email', 'nazzy@local.dev' )
             .then( user => {
                 assert.isOk(user, true);
@@ -58,6 +58,60 @@ describe('MySQL: User Query', () => {
         deleteUser(userId)
             .then(ok => {
                 assert.isOk(ok, true);
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Should get multiple users with no filter', async () => {
+        let users = ['nash'];
+
+        for ( let i = 1; i < 6; i++ ) {
+            let user = users + i;
+
+            await addUser({
+                display: user,
+                email: user + '@local.dev',
+                pass: user + '12345'
+            }).catch(returnFalse);
+        }
+
+        return getUsers()
+            .then( results => {
+                assert.isArray(results);
+            });
+    });
+
+    it('Should get multiple users of the same group', done => {
+        getUsers({
+            group: 'subscriber'
+        })
+            .then( results => {
+                assert.isOk( results, true );
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Should get 3 users of the same group', done => {
+        getUsers({
+            page: 1,
+            perPage: 3,
+            group__in: ['subscriber']
+        })
+            .then( results => {
+                assert.equal(3, results.length );
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Should return an array of users ID', done => {
+        getUsers({
+            fields: 'IDs'
+        })
+            .then( results => {
+                assert.isArray(results);
                 done();
             })
             .catch(done);
